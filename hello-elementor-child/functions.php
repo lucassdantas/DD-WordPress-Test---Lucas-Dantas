cd71228e38231e6d39c4c8451e82212a0b2dfc27
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-06-18 19:39:29 -0300

    finished child theme

---
+++ hello-elementor-child/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+defined('ABSPATH') or die('Not allowed');
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+function enqueue_parent_styles() {
+  wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+}
+
